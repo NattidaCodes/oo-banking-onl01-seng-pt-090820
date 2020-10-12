e2312f2ab909edd9cb8132704144ef7df6fd4f70
Nattida Wong <nattidac.wong@gmail.com>
2020-10-12 19:39:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @sender.balance < amount && @status = "rejected"
+      @sender.balance < @amount && @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
